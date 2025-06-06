142400c2ba74cce36cdd349c0ed348f616341850
Eremeev-O <samprotez63@gmail.com>
2025-06-06 12:55:47 +0400

    Home work OOP1

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,34 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+
+public class App {
+    public static void main(String[] args) {
+        ProductBasket productBasket = new ProductBasket();
+
+        System.out.println("Вводим продукты в корзину:");
+        productBasket.addProduct("Мороженое", 100);
+        productBasket.addProduct("Пироженое", 110);
+        productBasket.addProduct("Твороженое", 120);
+        productBasket.addProduct("Печенье", 30);
+        productBasket.addProduct("Конфеты", 200);
+        productBasket.addProduct("Семечки", 60);
+        productBasket.addProduct("Семечки", 90);
+
+
+
+        System.out.println("Выводим перечень продуктов в корзине");
+        productBasket.printAllProductBasket();
+
+        System.out.println("Ищем Семечки - " + productBasket.findProduct("Семечки"));
+
+        System.out.println("Очищаем корзину");
+        productBasket.basketCleaning();
+        System.out.println("Выводим перечень продуктов в корзине");
+        productBasket.printAllProductBasket();
+        System.out.println("Считаем стоимость товара в пустой корзине: " + productBasket.basketCost());
+        System.out.println("Ищем товар в пустой корзине. Например: Спички = " + productBasket.findProduct("Спички"));
+
+
+    }
+}
---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,62 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private final Product[] products;
+
+    public ProductBasket() {
+        this.products = new Product[5];
+    }
+
+    public void addProduct(String name, int cost) {
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i] == null) {
+                Product newProduct = new Product(name, cost);
+                this.products[i] = newProduct;
+                return;
+            }
+        }
+        System.out.println("Невозможно добавить продукт");
+    }
+    public void printAllProductBasket() {
+        int num = 0;
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i] != null) {
+                Product product = this.products[i];
+                System.out.println(this.products[i].toString());
+            } else {
+                num++;
+            }
+        }
+        if (num != this.products.length) {
+            System.out.println("Итого: " + basketCost());
+        } else {
+            System.out.println("в корзине пусто");
+        }
+    }
+    public int basketCost() {
+        int summ = 0;
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i] != null) {
+                summ += this.products[i].getCost();
+            }
+        }
+        return summ;
+    }
+    public boolean findProduct (String name) {
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i] != null && this.products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void basketCleaning() {
+        for (int i = 0; i < this.products.length; i++) {
+            this.products[i] = null;
+        }
+    }
+
+}
